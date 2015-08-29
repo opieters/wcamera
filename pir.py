9773e296f1eeb990a79e8786ba2bf3ca84ebe9f6
Olivier Pieters <olivier.pieters@outlook.com>
2015-08-29 11:02:42 +0200

    Bug fix.

--- pir.py
+++ pir.py
@@ -3,17 +3,24 @@ import time
 import picamera
 import threading
 
-def PIR_motion_detected(pin):
+def PIR_motion_detected():
     print("Motion detected")
     camera.start_recording("myvideo.h264")
 
-def PIR_no_motion(pin):
+def PIR_no_motion():
     print("No motion")
     threading.Timer(10, stop_if_no_motion).start()
 
 def stop_if_no_motion():
     if no_motion:
         camera.stop_recording()
+
+def PIR_edge(pin):
+    if GPIO.input(pin): # rising
+        PIR_motion_detected()
+    else: # falling
+        PIR_no_motion()
+
 
 # set GPIO pin numbering
 GPIO.setmode(GPIO.BCM)
@@ -31,8 +38,7 @@ camera.resolution = (640, 480)
 try:
     print("Press CTRL-C to exit script")
     time.sleep(1)
-    GPIO.add_event_detect(PIR_pin,GPIO.RISING,callback=PIR_motion_detected)
-    GPIO.add_event_detect(PIR_pin,GPIO.FALLING,callback=PIR_no_motion)
+    GPIO.add_event_detect(PIR_pin,GPIO.BOTH,callback=PIR_edge)
     while 1:
         time.sleep(100)
 except KeyboardInterrupt:
