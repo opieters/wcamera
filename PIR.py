9abc0f9d2541f3439cc2f6682980d2465d083391
Olivier Pieters <olivier.pieters@outlook.com>
2015-08-29 18:18:59 +0200

    Updated file naming + terminate trigger + duration + documented code.

--- PIR.py
+++ PIR.py
@@ -1,16 +1,28 @@
 import RPi.GPIO as GPIO
-import time
+import time, datetime
 import picamera
 import threading
 
-# set GPIO pin numbering
-GPIO.setmode(GPIO.BCM)
+"""PIR implements motion detection based on the readouts from a PIR sensor.
+
+In order to correcly function, the following GPIO pin number parameter must be
+set *before* the actual detection starts with: `PIR.gpio_pin`.
+
+Other global paramters are:
+* `camera`: Raspberry Pi camera module object, set to a resolution of 640x480 by
+ default. The camera object should not changed. Camera perameters can be
+ modified.
+* `stop_detection_pin`: GPIO pin number associated with terminate signal. A
+ rising or falling edge event ends motion detection.
+* `duration`: how long motion detection should run in seconds (0 = infinity)
+* `motion`: keeps track of ongoing motion detection (do NOT modify!)
+* `run_complete`: flag if motion detection should terminate (do NOT modify!)
+
+Note: do not load with the `from ... import *` syntax to avoid naming conflicts.
+"""
 
 # PIR GPIO pin
-gpio_pin = 7
-
-# register GPIO pin
-GPIO.setup(gpio_pin,GPIO.IN)
+gpio_pin = 26
 
 # camera
 camera = picamera.PiCamera()
@@ -22,10 +34,16 @@ motion = False
 # duration variable
 run_complete = False
 
+# register specific pin to prematurely end detection on GPIO event
 stop_detection_pin = -1
 
+# set run duration (0 = run infinitly long)
+duration = 0
+
 def motion_detected(video_name):
-    print("Motion detected")
+    """Callback if motion is detected. A video will be created with name
+    `video_name`."""
+    print("[INFO] Motion detected")
     recording = True
     motion = True
     if not recording:
@@ -33,27 +51,34 @@ def motion_detected(video_name):
 
 
 def no_motion():
-    print("No motion")
+    """Callback if motion ended."""
+    print("[INFO] Motion ended")
     motion = False
     threading.Timer(10, stop_recording).start()
 
 def stop_recording():
+    """Stops camera recording (if in progress)."""
     if (not motion) and recording:
         camera.stop_recording()
         recording = False
 
 def gpio_event(pin):
+    """GPIO event callback."""
     if GPIO.input(pin): # rising
-        motion_detected()
+        motion_detected("motion-" + datetime.datetime.now().strftime("%x-%X") + ".h264")
     else: # falling
         no_motion()
 
 def run_timer_callback(pin):
+    """Callback to end motion detection after `duration` seconds"""
     run_complete = True
 
 def run():
-    run_complete = False
-    threading.Timer(duration, run_timer_callback).start()
+    """Perform motion detecton."""
+    GPIO.setup(gpio_pin, GPIO.IN) # register GPIO pin
+    run_complete = False # run flag
+    if duration > 0: # stop duration after specified time
+        threading.Timer(duration, run_timer_callback).start()
     try:
         print("[INFO] Press CTRL-C to exit run function prematurely")
         time.sleep(1)
