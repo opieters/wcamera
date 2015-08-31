d081d0cf607528c444b4b28e2111e1c1fa09e1e6
Olivier Pieters <olivier.pieters@outlook.com>
2015-08-31 21:46:34 +0200

    Used config file now. Bug fixes.

--- PIR.py
+++ PIR.py
@@ -1,18 +1,16 @@
 import RPi.GPIO as GPIO
-import time, datetime
-import picamera
-import threading
+import time, datetime, json
+import picamera import PiCamera
+import threading import Timer
+import argparse import ArgumentParser
 
 """PIR implements motion detection based on the readouts from a PIR sensor.
-
-In order to correcly function, the following GPIO pin number parameter must be
-set *before* the actual detection starts with: `PIR.gpio_pin`.
 
 Other global paramters are:
 * `camera`: Raspberry Pi camera module object, set to a resolution of 640x480 by
  default. The camera object should not changed. Camera perameters can be
  modified.
-* `stop_detection_pin`: GPIO pin number associated with terminate signal. A
+* `stop detection GPIO pin`: GPIO pin number associated with terminate signal. A
  rising or falling edge event ends motion detection.
 * `duration`: how long motion detection should run in seconds (0 = infinity)
 * `motion`: keeps track of ongoing motion detection (do NOT modify!)
@@ -21,24 +19,26 @@ Other global paramters are:
 Note: do not load with the `from ... import *` syntax to avoid naming conflicts.
 """
 
-# PIR GPIO pin
-gpio_pin = 26
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--conf", required=False, help="Path to json configuration file", default="conf.json")
+
+    args = vars(parser.parse_args())
+    conf_file = args["conf"]
+else:
+    conf_file = "conf.json"
+
+conf = json.load(open(conf_file))
 
 # camera
-camera = picamera.PiCamera()
-camera.resolution = (640, 480)
+camera = PiCamera()
+camera.resolution = tuple(conf["resolution"])
 
 # motion variable
 motion = False
 
 # duration variable
 run_complete = False
-
-# register specific pin to prematurely end detection on GPIO event
-stop_detection_pin = -1
-
-# set run duration (0 = run infinitly long)
-duration = 0
 
 def motion_detected(video_name):
     """Callback if motion is detected. A video will be created with name
@@ -70,21 +70,24 @@ def gpio_event(pin):
         no_motion()
 
 def run_timer_callback(pin):
-    """Callback to end motion detection after `duration` seconds"""
+    """Callback to end motion detection after `conf["duration"]` seconds"""
     run_complete = True
 
 def run():
     """Perform motion detecton."""
-    GPIO.setup(gpio_pin, GPIO.IN) # register GPIO pin
+    GPIO.setup(conf["PIR GPIO pin"], GPIO.IN) # register GPIO pin
     run_complete = False # run flag
-    if duration > 0: # stop duration after specified time
-        threading.Timer(duration, run_timer_callback).start()
+    if conf["duration"] > 0: # stop recording after specified time
+        Timer(conf["duration"], run_timer_callback).start()
+    else:
+        run_complete = False
     try:
         print("[INFO] Press CTRL-C to exit run function prematurely")
         time.sleep(1)
-        GPIO.add_event_detect(gpio_pin, GPIO.BOTH, callback=gpio_event)
-        if stop_detection_pin >= 0:
-            GPIO.add_event_detect(gpio_pin, GPIO.BOTH, callback=run_timer_callback)
+        GPIO.add_event_detect(conf["PIR GPIO pin"], GPIO.BOTH, callback=gpio_event)
+        if conf["stop detection GPIO pin"] >= 0:
+            GPIO.setup(conf["stop detection GPIO pin"], GPIO.IN)
+            GPIO.add_event_detect(conf["stop detection GPIO pin"], GPIO.BOTH, callback=run_timer_callback)
         while 1:
             time.sleep(100)
     except KeyboardInterrupt or run_complete:
